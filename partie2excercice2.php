ae8e6cdc2477a6947ecf7e0b8d99450f143a49a3
Georgesunderscore <Georgeskayal2014@gmail.com>
2023-01-20 15:54:09 +0100

    test

--- partie2excercice2.php
+++ partie2excercice2.php
@@ -12,22 +12,48 @@
     <p>Réaliser un algorithme permettant d’afficher le tableau HTML suivant <br>
     (notez que le nom du pays s’affichera en majuscule et que le tableau est trié par ordre alphabétique du nom de pays)<br>
      grâce à une fonction personnalisée.<br>
+     Vous devrez appeler la fonction comme suit : afficherTableHTML($capitales);
 
-        Vous devrez appeler la fonction comme suit : convertirMajRouge($texte) ;   </p>
+    </p>
     <h2>Résultat 
-        Affichage (si $texte = « Mon texte en paramètre »)
-        MON TEXTE EN PARAMETRE en rouge
+    Affichage
+    Pays Capitale<br>
+    ALLEMAGNE Berlin<br>
+    FRANCE Paris<br>
+    ITALIE Rome<br>
+    USA Washington<br>
     </h2>
     
     <?php 
-    $text = " Mon texte en paramètre ";
-    echo convertirMajRouge($text);
-    //echo convertirMajRouge("TEST");
-    function convertirMajRouge($texte):string{
-        $result = mb_strtoupper($texte) ;
-        return "<span class='h1red'>" . $result . "</span>";
+       $capitales = array ("France"=>"Paris",
+                           "Allemagne"=>"Berlin",
+                           "USA"=>"Washington",
+                           "Italie"=>"Rome");
 
-    }
+        function afficherTableHTML($capitales):void{
+
+            //sort par clé
+            ksort($capitales);
+            
+            foreach($capitales as $key => $value){
+                echo "<span class='h1red'>" . mb_strtoupper($key) . "</span>";    
+                echo " ";
+                echo "<span class='h1red'>" . $value . "</span>";    
+                
+                echo "<br>";
+            }
+            
+            
+
+
+
+
+            
+
+            };
+    
+        echo     afficherTableHTML($capitales) ; 
+   
 
     
 
